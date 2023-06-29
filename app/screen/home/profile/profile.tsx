4e630c2736a4559cd88236ce0fc8e5ad7d884cac
nyujuzer <sallairobert94@gmail.com>
2023-06-29 23:00:41 +0200

    Gon' try something fuggin stupid, so... quicksave

--- app/screen/home/profile/profile.tsx
+++ app/screen/home/profile/profile.tsx
@@ -1,32 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Image, Button , Text} from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import { ip } from '../../../components/helpers/conf';
-import xhtmlrequestBuilder from '../../../components/helpers/request';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { Avatar, Button } from 'react-native-paper';
+import { createAvatar } from '@dicebear/core';
+import { lorelei } from '@dicebear/collection';
+import {useNavigation} from '@react-navigation/native'
+const ProfileScreen = (props) => {
+  const showPrompt = true; // Set to true if the profile picture is not available
+  const [profilePicture, setImage] = useState<any>();
+  const navigation = useNavigation();
 
-const requestUserDetails = (id) =>{
-  const x = new xhtmlrequestBuilder()
-  x.to(ip).atRoute("").setHeaders({"Content-Type":"application/json"})
-}
-const ProfileScreen = ({uid}) => {
-  useEffect(()=>{
-    requestUserDetails(uid)
-  },  [])
- return (
-  <View>
-  </View>
- )
+  const renderProfilePicture = () => {
+    const avatar = createAvatar(lorelei, {
+      seed: 'Kitty',
+      // ... other options
+    });
+    return (
+      <View>
+        {/* Render the avatar component */}
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      {renderProfilePicture()}
+
+      {showPrompt && (
+        <View style={styles.promptContainer}>
+          <Text style={styles.promptText}>Please complete your signup</Text>
+          <Button
+            mode="contained"
+            onPress={() => navigation.navigate("Finish")}
+          >
+            Finish Signup
+          </Button>
+          <Button onPress={()=>{}}>
+
+          </Button>
+        </View>
+      )}
+    </View>
+  );
 };
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20,
   },
-  image: {
-    width: '100%',
-    height: 300,
+  profileImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+  },
+  diceBearAvatar: {
+    backgroundColor: '#FFC107',
+    marginVertical: 20,
+  },
+  promptContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  promptText: {
+    fontSize: 16,
+    marginBottom: 10,
   },
 });
 
