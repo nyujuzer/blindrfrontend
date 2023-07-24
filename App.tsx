09a215c69f945458045fa0550cca3ba4672bd204
nyujuzer <sallairobert94@gmail.com>
2023-07-24 21:37:01 +0200

    test

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { Provider as PaperProvider } from "react-native-paper"
 import AppNavigator from "./app/components/app.navigator"
 
+//git
+
 export default function App() {
   return(
     <PaperProvider >
